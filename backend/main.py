ddc0d3382a44f33ac134d8798647f236306d98b5
Asim Sudheer <asimsudheer512@gmail.com>
2026-09-12 20:00:21 +0530

    cleaned the test files

--- backend/main.py
+++ backend/main.py
@@ -23,7 +23,7 @@ async def upload_file(file: UploadFile = File(...),job_description: str = Form(.
     with open(file_path,"wb") as f:
         f.write(await file.read())
     resume_text = extract_text_from_pdf(file_path)
-    #resume_generated_content = create_json(resume_text,API_KEY) 
+     
 
     jd_text = job_description.strip()
 
@@ -31,27 +31,3 @@ async def upload_file(file: UploadFile = File(...),job_description: str = Form(.
 
     return ats_score
 
-'''class JobDescription(BaseModel):
-    text : str
-
-class ATSRequest(BaseModel):
-    resume_text: str
-    job_description: str
-
-@app.post("/upload_jd/")
-async def paste_job_description(data: JobDescription):
-    char_length = len(data.text)
-    raw_text = data.text.strip()
-    job_desc = extract_text_from_pdf(raw_text) if raw_text.endswith('.pdf') else raw_text
-    return {
-        "status": "success",
-        "job_description": job_desc,
-        "length": char_length,
-
-    }
-
-@app.post("/ats_check/")
-async def ats_checking(data: ATSRequest):
-    main_result = perform_ats_check(data.resume_text, data.job_description, API_KEY)
-
-    return main_result'''
